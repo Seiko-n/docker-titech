59b52b92e2a37e67bc540351317567594ff01b67
H <h@HnoMacBook-Pro.local>
2017-10-23 19:07:59 +0900

    もう一度修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   get 'application' => 'application#index'
-=======
->>>>>>> e6fd0af14712eb47d06304fa62c527708947d6cd
 end
