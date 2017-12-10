8f78dee634e81421481f0ea77c80e8508177fc9e
kentaromatsuura <1122805.km.1020@gmail.com>
2017-12-10 12:04:14 +0900

    コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'application' => 'application#index'
   get 'home' => 'home#index'
   get 'haruki' => 'haruki#index'
+  get 'createrpage' => 'createrpage#index'
   root 'application#index'
 end
