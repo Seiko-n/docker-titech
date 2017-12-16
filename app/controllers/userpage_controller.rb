fc9ab6e67d0e1ab88594cd776897b9414b331eaa
netuseikou@gmail.com <Seiko-n@users.noreply.github.com>
2017-12-16 10:40:41 +0900

    トークン数計算できたよ💒

--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -1,2 +1,11 @@
 class UserpageController < ApplicationController
+  def index
+    @tests = User.all
+    @movies = Movie.all
+    @tokens = Token.all
+    @sum = 0
+    5.times do |i|
+      @sum =@sum + Token.find(i+1).aglt
+    end
+  end
 end
