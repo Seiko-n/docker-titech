a3c6c469379e28de4b1af5f9786b0d7a7b85fa46
kentaromatsuura <1122805.km.1020@gmail.com>
2017-12-16 12:36:53 +0900

    春樹へ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
-	def after_sign_in_path_for(resource)
-        userpage_path
-    end
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    private
-        def sign_in_required
-            redirect_to new_user_session_url unless user_signed_in?
-        end
-    
-    def after_sign_out_path_for(resource_or_scope)
-    	root_path
+  # 省略
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20171215063752) do
   enable_extension "plpgsql"
 
   create_table "creators", force: :cascade do |t|
+    t.string "moviegenre"
+    t.string "indtroduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,7 +32,6 @@ ActiveRecord::Schema.define(version: 20171215063752) do
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "creator_id"
     t.string "prefecture"
     t.string "sex"
     t.integer "age"
@@ -50,12 +51,31 @@ ActiveRecord::Schema.define(version: 20171215063752) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_type"
-    t.string "name"
-    t.string "password"
-    t.string "email"
+    t.string "user_type", default: "viewer"
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "profiles", "users"
