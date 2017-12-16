b40b4c791f6a3b5bb09df0013bd85fd053cce904
netuseikou@gmail.com <Seiko-n@users.noreply.github.com>
2017-12-16 12:22:30 +0900

    graph使いたい

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ end
 gem 'devise'
 gem 'omniauth-twitter'
 
+#グラフ描画
+gem "chartkick"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -1,3 +1,4 @@
+require 'time'
 class UserpageController < ApplicationController
   def index
     @tests = User.all
@@ -7,5 +8,10 @@ class UserpageController < ApplicationController
     5.times do |i|
       @sum =@sum + Token.find(i+1).aglt
     end
+    @passtime = (Time.now - @tokens.find(1).created_at).to_i
+
+    # @pie_chart = {@tokens.find(2).creator_id => Token.find(2).aglt, @tokens.find(3).creator_id => Token.find(3).aglt, @tokens.find(4).creator_id => Token.find(4).aglt, @tokens.find(5).creator_id => Token.find(5).aglt}
+    # @pie_chart = {'2014-04-01' => 20, '2014-04-02' => 50, '2014-04-03' => 30}
+
   end
 end
