390bfe61021dcaee0476907e29a2713faeb1d022
netuseikou@gmail.com <Seiko-n@users.noreply.github.com>
2017-12-17 08:11:37 +0900

    ranking

--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -6,12 +6,13 @@ class UserpageController < ApplicationController
     @movies = Movie.all
     @tokens = Token.all
     @sum = 0
+
     5.times do |i|
       @sum =@sum + Token.find(i+1).aglt
     end
+
     @passtime = (Time.now - @tokens.find(1).created_at).to_i
 
-    @pie_chart = {@tests.find(2).name => Token.find(2).aglt, @tests.find(3).name => Token.find(3).aglt, @tests.find(4).name => Token.find(4).aglt, @tests.find(5).name => Token.find(5).aglt}# @pie_chart = {'2014-04-01' => 20, '2014-04-02' => 50, '2014-04-03' => 30}
 
   end
 end
