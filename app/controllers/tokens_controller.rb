13a6f4c1f95af912bda2b265f6f0dba40f45a296
H <h@HnoMacBook-Pro-2.local>
2017-12-17 15:50:19 +0900

    ボタンを実装

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -11,14 +11,33 @@ class TokensController < ApplicationController
   def create
     @token = Token.create(params_action)
     @token.save
-    redirect_to tokens_path
+    @user = User.find(@token.creator_id)
+    puts("獲得前")
+    puts(@user.kakutoku)
+    @user.kakutoku += @token.aglt
+    puts("獲得後")
+    puts(@user.kakutoku)
+    @user.save
+
+    @user = User.find(@token.user_id)
+    puts("支払い前")
+    puts(@user.syojikin)
+    @user.syojikin -= @token.aglt
+    @user.save
+    puts("獲得後")
+    puts(@user.syojikin)
+
+    redirect_to userpage_path
 
   end
 
   private
   def params_action
-    params.require(:token).permit(:user_id,:creator_id,:aglt)
+    params.require(:token).permit(:user_id,:creator_id,:aglt,:movie_id)
     #ここで許可したデータのみ書き込みが許される。
   end
+
+  helper_method :new
+  helper_method :create
 
 end
